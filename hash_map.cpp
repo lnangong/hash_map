dd20ccf98cd2ee1d93bdbc304f30204d3cd87e1b
lnangong <lnangong@hawk.iit.edu>
2014-07-17 13:19:23 -0500

    hash_map.cpp update

--- hash_map.cpp
+++ hash_map.cpp
@@ -1,6 +1,13 @@
 #ifndef _HASH_MAP_H_INCLUDED_
 #define _HASH_MAP_H_INCLUDED_
 
+typedef struct List
+{
+        int value;            //list value
+        struct List* next;    //next list
+        struct List* prev;    //previous list
+}List;
+
 template <class T>
 class hash_map { 
 
