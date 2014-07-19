00d0db3511d6dfcaf92f4e95b364306f2f1e2c6f
lnangong <lnangong@hawk.iit.edu>
2014-07-19 16:50:36 -0500

    iterator update

--- iterator.cpp
+++ iterator.cpp
@@ -1,5 +1,16 @@
 
 #include "iterator.h"
 
+//typedef hash_map::iterator<Key,Value> HashIterator;
+
+HashIterator& HashIterator::operator++(){
+	item = item->next;
+	return *this;
+}
+
+HashIterator& HashIterator::operator--(){
+        item = item->prev;
+        return *this;
+}
 
 
--- iterator.h
+++ iterator.h
@@ -3,12 +3,17 @@
 #define _ITERATOR_H_
 
 #include "hash_map.h"
+#include "node.h"
+
 
 
 template <typename Key, typename Value>
 class hash_map<Key,Value>::iterator{
 
 private:
+	typedef node<Key,Value> Node;
+	Node* item;
+	iterator(Node* item);
 
 public:
         iterator& operator++();
