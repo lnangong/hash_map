090138e894af5035b80b8e587fe838faea4c9d3e
lnangong <lnangong@hawk.iit.edu>
2014-07-21 15:02:41 -0500

    insert update

--- hash_map.cpp
+++ hash_map.cpp
@@ -63,25 +63,61 @@ iterator hash_map::end(){	return iterator(trailer);	}
 template <typename Key, typename Value>
 typename hash_map<Key,Valu>::iterator 
 iterator insert (const Key& key, const Value& value){
-	int index = hash(key);
-	entry* bucket = new entry;
+	size_t index = hash(key);		//Get hash index
+	entry* bucket = new entry;		//Create new bucket
 	bucket->_key = key;
 	bucket->_value = value;
-
-	if(HashTable[index]->next == NULL){
-		HashTable[index]->bucket;
-		bucket->prev = HashTable[index];
-		bucket->next = NULL;	
+	
+	if(Header->next == Trailer){		//Empty hash map
+		HashTable[index]->next = bucket;//Points to first bucket in the index chain
+		HashTable[index]->prev = bucket;//Points to last bucket in the index chain
+		Header->next = bucket;		//Header points to the first bucket in the hash map
+		bucket->prev = Header;		//First bucket's 'prev' points to the header
+		bucket->next = Trailer;		//First bucket's 'next' points to the trailer
+		Trailer->prev = bucket;		//Trailer points to the last bucket
 
 		return iterator(bucket);
 	}
-
-	bucket->prec = HashTable[index];
-	bucket->next = HashTable[index]->next;
-	HashTable[index]->next = bucket;
-
-	return iterator(bucket);
 	
+	//In case of header not points to trailer, hash map is not empty
+	if(HashTable[index]->next == NULL){		//Empty index chain
+		HashTable[index]->next = bucket;	
+                HashTable[index]->prev = bucket;	
+		if(index < hash[Header->next->_key]){	//In case of inserting bucket before the header
+			bucket->next = Header->next;
+			bucket->prev = Header;
+			Header->next->prev = bucket;
+			Header->next = bucket;
+		}
+		else if(index > hash[Header->next->_key] && index < hash[Trailer->prev->_key]){
+			//In case of inserting bucket between header and trailer
+			size_t prev_index = index;
+
+			while(HashTable[--prev_index]->next != NULL){}
+		
+			bucket->next = HashTable[prev_index]->prev->next;
+			bucket->prev = HashTable[prev_index]->prev;
+			HashTable[prev_index]->prev->next = bucket;
+			bucket->next->prev = bucket;
+		}
+		else{ //Insert bucket after the trailer
+			bucket->next = Trailer;
+                        bucket->prev = Trailer->prev;
+                        Trailer->prev = bucket;
+                        bucket->prev->next = bucket;	
+
+		}
+
+		return iterator(bucket);
+	}
+	
+	//Insert bucket to the index chain, which is not empty
+	bucket->next = HashTable[index]->next;		//Insert bucket in the first position in the index chain
+	bucket->prev = HashTable[index]->next->prev;
+	HashTable[index]->next = bucket;
+	
+	return iterator(bucket);
+
 }
 
 
@@ -89,7 +125,7 @@ iterator insert (const Key& key, const Value& value){
 template <typename Key, typename Value>
 typename hash_map<Key,Valu>::iterator 
 iterator find (const Key& key){
-	int index = hash(key);
+	size_t index = hash(key);
 	iterator iter(HashTable[index]);
 	
 	for( ; iter->_bucket->next != NULL; ++iter){
